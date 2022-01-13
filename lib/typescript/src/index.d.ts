3bb637325238b94c84ca12a0cbdc2882d23c1934
Aditya Pahilwani <aditya.pahilwani@groww.in>
2022-01-13 17:22:05 +0530

    gitpkg

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Navigators
+ */
+export { default as createNativeStackNavigator } from './navigators/createNativeStackNavigator';
+export { default as NativeStackView } from './views/NativeStackView';
+/**
+ * Types
+ */
+export type { NativeStackHeaderProps, NativeStackNavigationOptions, NativeStackNavigationProp, NativeStackScreenProps, NativeStackNavigationEventMap, NativeStackNavigatorProps, } from './types';
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * Navigators
+ */
+export { default as createNativeStackNavigator } from './navigators/createNativeStackNavigator';
+export { default as NativeStackView } from './views/NativeStackView';
+
+/**
+ * Types
+ */
+export type {
+  NativeStackHeaderProps,
+  NativeStackNavigationEventMap,
+  NativeStackNavigationOptions,
+  NativeStackNavigationProp,
+  NativeStackNavigatorProps,
+  NativeStackScreenProps,
+} from './types';
